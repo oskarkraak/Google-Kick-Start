1d69e1308dc8575c13cefa1107b1a26dd6ce0eef
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-10-15 17:08:31 +0200

    Improve template

--- template/Solution.java
+++ template/Solution.java
@@ -6,113 +6,116 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 public class Solution {
 
-	private static Scanner in;
+    private static Scanner in;
 
-	public static void main(String[] args) {
-		in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-		int T = readInt();
+    public static void main(String[] args) {
+        in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int T = readInt();
 
-		for (int x = 1; x <= T; x++) {
+        for (int x = 1; x <= T; x++) {
 
-		}
-	}
+            int output = 0;
 
-	/**
-	 * Reads one line, separates words
-	 */
-	private static String[] readWords() {
-		return readLine().split(" ");
-	}
+            out("Case #" + x + ": " + output);
+        }
+    }
 
-	/**
-	 * Reads one line of integers, separates them
-	 */
-	private static int[] readIntArr() {
-		return toIntArr(readWords());
-	}
+    /**
+     * Reads one line, separates words
+     */
+    private static String[] readWords() {
+        return readLine().split(" ");
+    }
 
-	/**
-	 * Converts a String[] to an int[]
-	 */
-	private static int[] toIntArr(String[] s) {
-		int[] intArr = new int[s.length];
-		for (int i = 0; i < s.length; i++)
-			intArr[i] = Integer.parseInt(s[i]);
-		return intArr;
-	}
+    /**
+     * Reads one line of integers, separates them
+     */
+    private static int[] readIntArr() {
+        return toIntArr(readWords());
+    }
 
-	/**
-	 * Reads a single integer and advances to the next line
-	 */
-	private static int readInt() {
-		int i = in.nextInt();
-		in.nextLine();
-		return i;
-	}
+    /**
+     * Converts a String[] to an int[]
+     */
+    private static int[] toIntArr(String[] s) {
+        int[] intArr = new int[s.length];
+        for (int i = 0; i < s.length; i++)
+            intArr[i] = Integer.parseInt(s[i]);
+        return intArr;
+    }
 
-	/**
-	 * Reads the next line
-	 */
-	private static String readLine() {
-		return in.nextLine();
-	}
+    /**
+     * Reads a single integer and advances to the next line
+     */
+    private static int readInt() {
+        int i = in.nextInt();
+        in.nextLine();
+        return i;
+    }
 
-	private static void out(Object s) {
-		System.out.println(s);
-	}
+    /**
+     * Reads the next line
+     */
+    private static String readLine() {
+        return in.nextLine();
+    }
 
-	/**
-	 * Checks if a given long is a palindrome by reversing it.
-	 */
-	private static boolean isPalindrome(long l) {
-		int num = i;
-		int reverse = 0;
-		while (num > 0) {
-			reverse = reverse * 10 + num % 10;
-			num /= 10;
-		}
-		if (reverse == i) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    private static void out(Object s) {
+        System.out.println(s);
+    }
 
-	/**
-	 * Removes the char at the spcified index from a String. Warning: Very slow!
-	 */
-	private static String removeChar(String s, int index) {
-		String result = "";
-		for (int i = 0; i < s.length(); i++)
-			if (i != index)
-				result += s.charAt(i);
-		return result;
-	}
+    /**
+     * Checks if a given long is a palindrome by reversing it.
+     */
+    private static boolean isPalindrome(long l) {
+        long num = l;
+        long reverse = 0;
+        while (num > 0) {
+            reverse = reverse * 10 + num % 10;
+            num /= 10;
+        }
+        if (reverse == l) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Removes the char at the spcified index from a String. Warning: Very slow!
-	 */
-	private static String setChar(String s, int index, char c) {
-		String result = "";
-		for (int i = 0; i < s.length(); i++)
-			if (i != index)
-				result += s.charAt(i);
-		return result;
-	}
+    /**
+     * Removes the char at the spcified index from a String. Warning: Very slow!
+     */
+    private static String removeChar(String s, int index) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++)
+            if (i != index)
+                result += s.charAt(i);
+        return result;
+    }
 
-	/**
-	 * Inserts the char at the spcified index in a String. Warning: Very slow!
-	 */
-	private static String insertChar(String s, int index, char c) {
-		String result = "";
-		for (int i = 0; i < s.length(); i++)
-			if (i != index)
-				result += s.charAt(i);
-			else {
-				result += c;
-				result += s.charAt(i);
-			}
-		return result;
-	}
+    /**
+     * Removes the char at the spcified index from a String. Warning: Very slow!
+     */
+    private static String setChar(String s, int index, char c) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++)
+            if (i != index)
+                result += s.charAt(i);
+        return result;
+    }
+
+    /**
+     * Inserts the char at the spcified index in a String. Warning: Very slow!
+     */
+    private static String insertChar(String s, int index, char c) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++)
+            if (i != index)
+                result += s.charAt(i);
+            else {
+                result += c;
+                result += s.charAt(i);
+            }
+        return result;
+    }
 
 }
